63535ae0aa9013e83408c49a27cb60241ddb1e3b
justin <happing51409@gmail.com>
2026-06-10 13:17:44 +0800

    Update ProjectContext.java

--- src/main/java/com/myproject/ProjectContext.java
+++ src/main/java/com/myproject/ProjectContext.java
@@ -1,7 +1,7 @@
 package com.myproject;
 
 public class ProjectContext {
-    // 💡 用一個靜態變數（static）來存放被選擇的影片路徑
+    // 用一個靜態變數（static）來存放被選擇的影片路徑
     private static String selectedVideoPath = "";
 
     public static void setSelectedVideoPath(String path) {
